f932b02c319b297aab61ebe2eda18c1f2a95a7d8
HunterHoBBQ <HunterHoBBQ@gmail.com>
2023-08-11 16:41:25 +0800

    v1.0.1 init

--- js/contact.js
+++ js/contact.js
@@ -1,159 +1,137 @@
 /*------------------------------------------------------
- Name: 		Discovery
- Version: 	1.0
- Updated:	24/04/2015
- Website:	http://audaindesigns.com
- Author:	Audain Designs
- Type:		JS - Contact Form
 -------------------------------------------------------*/
 
-(function($) {
+(function ($) {
+  "use strict";
 
-	'use strict';
-
-	/*
+  /*
 	Contact Form: Basic
 	*/
-	$('#contactForm:not([data-type=advanced])').validate({
-		submitHandler: function(form) {
+  $("#contactForm:not([data-type=advanced])").validate({
+    submitHandler: function (form) {
+      var $form = $(form),
+        $messageSuccess = $("#contactSuccess"),
+        $messageError = $("#contactError"),
+        $submitButton = $(this.submitButton);
 
-			var $form = $(form),
-				$messageSuccess = $('#contactSuccess'),
-				$messageError = $('#contactError'),
-				$submitButton = $(this.submitButton);
+      $submitButton.button("loading");
 
-			$submitButton.button('loading');
+      // Ajax Submit
+      $.ajax({
+        type: "POST",
+        url: $form.attr("action"),
+        data: {
+          name: $form.find("#name").val(),
+          email: $form.find("#email").val(),
+          subject: $form.find("#subject").val(),
+          message: $form.find("#message").val(),
+        },
+        dataType: "json",
+        complete: function (data) {
+          if (typeof data.responseJSON === "object") {
+            if (data.responseJSON.response == "success") {
+              $messageSuccess.removeClass("hidden");
+              $messageError.addClass("hidden");
 
-			// Ajax Submit
-			$.ajax({
-				type: 'POST',
-				url: $form.attr('action'),
-				data: {
-					name: $form.find('#name').val(),
-					email: $form.find('#email').val(),
-					subject: $form.find('#subject').val(),
-					message: $form.find('#message').val()
-				},
-				dataType: 'json',
-				complete: function(data) {
-				
-					if (typeof data.responseJSON === 'object') {
-						if (data.responseJSON.response == 'success') {
+              // Reset Form
+              $form.find(".form-control").val("").blur().parent().removeClass("has-success").removeClass("has-error").find("label.error").remove();
 
-							$messageSuccess.removeClass('hidden');
-							$messageError.addClass('hidden');
+              if ($messageSuccess.offset().top - 80 < $(window).scrollTop()) {
+                $("html, body").animate(
+                  {
+                    scrollTop: $messageSuccess.offset().top - 80,
+                  },
+                  300
+                );
+              }
 
-							// Reset Form
-							$form.find('.form-control')
-								.val('')
-								.blur()
-								.parent()
-								.removeClass('has-success')
-								.removeClass('has-error')
-								.find('label.error')
-								.remove();
+              $submitButton.button("reset");
 
-							if (($messageSuccess.offset().top - 80) < $(window).scrollTop()) {
-								$('html, body').animate({
-									scrollTop: $messageSuccess.offset().top - 80
-								}, 300);
-							}
+              return;
+            }
+          }
 
-							$submitButton.button('reset');
-							
-							return;
+          $messageError.removeClass("hidden");
+          $messageSuccess.addClass("hidden");
 
-						}
-					}
+          if ($messageError.offset().top - 80 < $(window).scrollTop()) {
+            $("html, body").animate(
+              {
+                scrollTop: $messageError.offset().top - 80,
+              },
+              300
+            );
+          }
 
-					$messageError.removeClass('hidden');
-					$messageSuccess.addClass('hidden');
+          $form.find(".has-success").removeClass("has-success");
 
-					if (($messageError.offset().top - 80) < $(window).scrollTop()) {
-						$('html, body').animate({
-							scrollTop: $messageError.offset().top - 80
-						}, 300);
-					}
+          $submitButton.button("reset");
+        },
+      });
+    },
+  });
 
-					$form.find('.has-success')
-						.removeClass('has-success');
-						
-					$submitButton.button('reset');
+  $("#subscribeForm:not([data-type=advanced])").validate({
+    submitHandler: function (form) {
+      var $form = $(form),
+        $messageSuccess = $("#subscribeSuccess"),
+        $messageError = $("#subscribeError"),
+        $submitButton = $(this.submitButton);
 
-				}
-			});
-		}
-	});
-	
-	$('#subscribeForm:not([data-type=advanced])').validate({
-		submitHandler: function(form) {
+      $submitButton.button("loading");
 
-			var $form = $(form),
-				$messageSuccess = $('#subscribeSuccess'),
-				$messageError = $('#subscribeError'),
-				$submitButton = $(this.submitButton);
+      // Ajax Submit
+      $.ajax({
+        type: "POST",
+        url: $form.attr("action"),
+        data: {
+          name: $form.find("#name").val(),
+          email: $form.find("#email").val(),
+          subject: $form.find("#subject").val(),
+          message: $form.find("#message").val(),
+        },
+        dataType: "json",
+        complete: function (data) {
+          if (typeof data.responseJSON === "object") {
+            if (data.responseJSON.response == "success") {
+              $messageSuccess.removeClass("hidden");
+              $messageError.addClass("hidden");
 
-			$submitButton.button('loading');
+              // Reset Form
+              $form.find(".form-control").val("").blur().parent().removeClass("has-success").removeClass("has-error").find("label.error").remove();
 
-			// Ajax Submit
-			$.ajax({
-				type: 'POST',
-				url: $form.attr('action'),
-				data: {
-					name: $form.find('#name').val(),
-					email: $form.find('#email').val(),
-					subject: $form.find('#subject').val(),
-					message: $form.find('#message').val()
-				},
-				dataType: 'json',
-				complete: function(data) {
-				
-					if (typeof data.responseJSON === 'object') {
-						if (data.responseJSON.response == 'success') {
+              if ($messageSuccess.offset().top - 80 < $(window).scrollTop()) {
+                $("html, body").animate(
+                  {
+                    scrollTop: $messageSuccess.offset().top - 80,
+                  },
+                  300
+                );
+              }
 
-							$messageSuccess.removeClass('hidden');
-							$messageError.addClass('hidden');
+              $submitButton.button("reset");
 
-							// Reset Form
-							$form.find('.form-control')
-								.val('')
-								.blur()
-								.parent()
-								.removeClass('has-success')
-								.removeClass('has-error')
-								.find('label.error')
-								.remove();
+              return;
+            }
+          }
 
-							if (($messageSuccess.offset().top - 80) < $(window).scrollTop()) {
-								$('html, body').animate({
-									scrollTop: $messageSuccess.offset().top - 80
-								}, 300);
-							}
+          $messageError.removeClass("hidden");
+          $messageSuccess.addClass("hidden");
 
-							$submitButton.button('reset');
-							
-							return;
+          if ($messageError.offset().top - 80 < $(window).scrollTop()) {
+            $("html, body").animate(
+              {
+                scrollTop: $messageError.offset().top - 80,
+              },
+              300
+            );
+          }
 
-						}
-					}
+          $form.find(".has-success").removeClass("has-success");
 
-					$messageError.removeClass('hidden');
-					$messageSuccess.addClass('hidden');
-
-					if (($messageError.offset().top - 80) < $(window).scrollTop()) {
-						$('html, body').animate({
-							scrollTop: $messageError.offset().top - 80
-						}, 300);
-					}
-
-					$form.find('.has-success')
-						.removeClass('has-success');
-						
-					$submitButton.button('reset');
-
-				}
-			});
-		}
-	});
-
+          $submitButton.button("reset");
+        },
+      });
+    },
+  });
 }).apply(this, [jQuery]);
